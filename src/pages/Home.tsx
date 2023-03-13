ab4e6f2b07ac6652b15edeb781c73ba0749c159a
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-03-13 12:24:26 -0300

    criação do componente de nossos serviços

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import Header from "../components/Header";
+import OurValues from "../components/OurValues";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <OurValues 
+        twoColumnTitle
+        title="Our Values"
+        bigTitle="Believe in innovation"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut"
+      />
       {/* jão mete bala */}
     </>
   );
